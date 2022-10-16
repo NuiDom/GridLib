cd8a5aae36ec61933d4585659bd81abe15d03712
nuidom <nuidom@hotmail.com>
2022-10-16 19:48:12 +0100

    Can reszie rects around P1 nad P2

--- griditem.cpp
+++ griditem.cpp
@@ -93,9 +93,24 @@ void gridItem::slotPointChanged(QPointF point)
     QPointF ppoint;
 
     //update patchroi
-    if(thisPoint == point2){
+    if(thisPoint == point1){
+        P1HasChanged(point);
+        pointChanged = true;
+    }
+    else if(thisPoint == point2){
         P2HasChanged(point);
         pointChanged = true;
+    }
+    else if(thisPoint == point3){
+        P3HasChanged(point);
+        pointChanged = true;
+    }
+    else if(thisPoint == point4){
+        P4HasChanged(point);
+        pointChanged = true;
+    }
+    else {
+        pointChanged = false;
     }
 ////    if(PatchROIs.contains(moveStartP)){
 //        for(int i=0; i<PatchROIs.size(); i++){
@@ -162,7 +177,31 @@ void gridItem::slotPointToChange(QPointF pointVal, currentPoint pointNum)
 
 void gridItem::P1HasChanged(QPointF point)
 {
+    for(int i=0; i<PatchROIs.size(); i++){
+        if((moveStartP.x()>(PatchROIs[i].P1.x()-7)) &&
+           (moveStartP.x()<(PatchROIs[i].P1.x()+7)) &&
+           (moveStartP.y()>(PatchROIs[i].P1.y()-7)) &&
+           (moveStartP.y()<(PatchROIs[i].P1.y()+7))   )
+        {
+            IndexOfChangingRect = i;
+        }
+    }
 
+    PatchROIs[IndexOfChangingRect].P1 = point;
+    PatchROIs[IndexOfChangingRect].P4 = QPointF(point.x(), PatchROIs[IndexOfChangingRect].P4.y());
+    PatchROIs[IndexOfChangingRect].P3 = QPointF(PatchROIs[IndexOfChangingRect].P3.x(), point.y());
+
+    PatchROIs[IndexOfChangingRect - 1].P3 = point;
+    PatchROIs[IndexOfChangingRect - 1].P2 = QPointF(point.x(), PatchROIs[IndexOfChangingRect - 1].P2.y());
+    PatchROIs[IndexOfChangingRect - 1].P1 = QPointF(PatchROIs[IndexOfChangingRect - 1].P1.x(), point.y());
+
+    PatchROIs[IndexOfChangingRect - numCols].P4 = point;
+    PatchROIs[IndexOfChangingRect - numCols].P1 = QPointF(point.x(), PatchROIs[IndexOfChangingRect - numCols].P1.y());
+    PatchROIs[IndexOfChangingRect - numCols].P2 = QPointF(PatchROIs[IndexOfChangingRect - numCols].P2.x(), point.y());
+
+    PatchROIs[IndexOfChangingRect - numCols - 1].P2 = point;
+    PatchROIs[IndexOfChangingRect - numCols - 1].P3 = QPointF(point.x(), PatchROIs[IndexOfChangingRect - numCols - 1].P3.y());
+    PatchROIs[IndexOfChangingRect - numCols - 1].P4 = QPointF(PatchROIs[IndexOfChangingRect - numCols - 1].P4.x(), point.y());
 }
 
 void gridItem::P2HasChanged(QPointF point)
@@ -176,6 +215,7 @@ void gridItem::P2HasChanged(QPointF point)
             IndexOfChangingRect = i;
         }
     }
+
     PatchROIs[IndexOfChangingRect].P2 = point;
     PatchROIs[IndexOfChangingRect].P3 = QPointF(point.x(), PatchROIs[IndexOfChangingRect].P3.y());
     PatchROIs[IndexOfChangingRect].P4 = QPointF(PatchROIs[IndexOfChangingRect].P4.x(), point.y());
@@ -195,11 +235,27 @@ void gridItem::P2HasChanged(QPointF point)
 
 void gridItem::P3HasChanged(QPointF point)
 {
-
+    for(int i=0; i<PatchROIs.size(); i++){
+        if((moveStartP.x()>(PatchROIs[i].P3.x()-7)) &&
+           (moveStartP.x()<(PatchROIs[i].P3.x()+7)) &&
+           (moveStartP.y()>(PatchROIs[i].P3.y()-7)) &&
+           (moveStartP.y()<(PatchROIs[i].P3.y()+7))   )
+        {
+            IndexOfChangingRect = i;
+        }
+    }
 }
 
 void gridItem::P4HasChanged(QPointF point)
 {
-
+    for(int i=0; i<PatchROIs.size(); i++){
+        if((moveStartP.x()>(PatchROIs[i].P4.x()-7)) &&
+           (moveStartP.x()<(PatchROIs[i].P4.x()+7)) &&
+           (moveStartP.y()>(PatchROIs[i].P4.y()-7)) &&
+           (moveStartP.y()<(PatchROIs[i].P4.y()+7))   )
+        {
+            IndexOfChangingRect = i;
+        }
+    }
 }
 
